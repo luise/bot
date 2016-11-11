b80b9334162f0d1aaa4e829e254db249b9f25d3f
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-11-10 22:14:55 -0500

    Working on the quilt spec

---
+++ quilt_spec.js
@@ -0,0 +1,26 @@
+function deploy(slack_channel, slack_endpoint, github_oath) {
+    service = new Service("bot", new Container("quilt/star-bot").setEnv({
+        "SLACK_CHANNEL": slack_channel,
+        "SLACK_ENDPOINT": slack_endpoint,
+        "GITHUB_OATH": github_oath,
+    }));
+
+    service.connect(80, publicInternet);
+    service.connect(53, publicInternet);
+
+    var namespace = createDeployment({
+        namespace: "quilt-star-bot",
+        adminACL: ["local"],
+    });
+
+    var baseMachine = new Machine({
+        provider: "Amazon",
+        cpu: new Range(1),
+        ram: new Range(1),
+        sshKeys: githubKeys("ejj"),
+    });
+
+    // Boot VMs with the properties of `baseMachine`.
+    namespace.deploy(baseMachine.asMaster());
+    namespace.deploy(baseMachine.asWorker());
+}
