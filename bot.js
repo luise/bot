e78b498614beccc4a77ccfcd3701ecf582ac434e
Luise Valentin <luise@quilt.io>
2017-05-28 17:50:35 -0700

    stars: Stop tracking stars
    Quilt-bot is now only responsible for requesting PR reviews.

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const {Container, Service, publicInternet} = require("@quilt/quilt");
 
 exports.New = function(slack_channel, slack_endpoint, github_oath) {
     service = new Service("bot", [new Container("quilt/bot").withEnv({
-        "SLACK_CHANNEL": slack_channel,
-        "SLACK_ENDPOINT": slack_endpoint,
         "GITHUB_OATH": github_oath,
     })]);
 
--- main.go
+++ main.go
@@ -12,11 +12,6 @@ import (
 	"github.com/google/go-github/github"
 )
 
-type starGazer struct {
-	username  string
-	avatarURL string
-}
-
 func main() {
 	fmt.Println("Started!")
 
@@ -40,113 +35,6 @@ func main() {
 			runReview(client)
 		}
 	}()
-
-	var users []starGazer
-	for range time.Tick(15 * time.Second) {
-		new, err := getUsers(client)
-		if err != nil {
-			fmt.Println("Failed to get stargazers: ", err)
-			continue
-		}
-
-		if users != nil {
-			runStarCheck(users, new)
-		}
-		users = new
-	}
-}
-
-func runStarCheck(users, new []starGazer) {
-	additions := extraStargazer(users, new)
-	deletions := extraStargazer(new, users)
-
-	total := len(new)
-
-	for _, add := range additions {
-		fmt.Println("New Star!", add.username)
-		post(add, total, true)
-	}
-
-	for _, del := range deletions {
-		fmt.Println("Lost a Star!", del.username)
-		post(del, total, false)
-	}
-}
-
-func post(user starGazer, total int, add bool) {
-	iconemoji := ":confetti_ball:"
-	color := "#009900" // Green
-	if !add {
-		iconemoji = ":slightly_frowning_face:"
-		color = "#D00000" // Red
-	}
-
-	title := "We've got a new star!"
-	if !add {
-		title = "We've lost a star!"
-	}
-
-	un := user.username
-	text := fmt.Sprintf(
-		"<https://github.com/NetSys/quilt/stargazers|%d Quilt Stargazers>\n\n",
-		total)
-	text += fmt.Sprintf("<https://github.com/%s|%s>", un, un)
-
-	slack(os.Getenv("SLACK_ENDPOINT"), slackPost{
-		Channel:   os.Getenv("SLACK_CHANNEL"),
-		Color:     color,
-		Username:  "quilt-bot",
-		Iconemoji: iconemoji,
-		Fields: []message{
-			{
-				Title: title,
-				Short: false,
-				Value: text,
-			},
-		},
-	})
-}
-
-// Returns all stargazers in `check` that are not in `base`.
-func extraStargazer(base, check []starGazer) []starGazer {
-	baseMap := map[string]struct{}{}
-	for _, b := range base {
-		baseMap[b.username] = struct{}{}
-	}
-
-	var extras []starGazer
-	for _, c := range check {
-		if _, ok := baseMap[c.username]; !ok {
-			extras = append(extras, c)
-		}
-	}
-	return extras
-}
-
-func getUsers(client *github.Client) ([]starGazer, error) {
-	var results []starGazer
-
-	opt := &github.ListOptions{}
-	for {
-		sgs, resp, err := client.Activity.ListStargazers(ctx(), "quilt", "quilt", opt)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, s := range sgs {
-			results = append(results, starGazer{
-				username:  *s.User.Login,
-				avatarURL: *s.User.AvatarURL,
-			})
-		}
-
-		if resp.NextPage == 0 {
-			break
-		}
-		opt.Page = resp.NextPage
-	}
-
-	return results, nil
 }
 
 func ctx() context.Context {
