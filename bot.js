d7ba322afc67b7499e4b6db0e91e74c530244b02
Kevin Lin <kevin@quilt.io>
2017-08-04 08:59:43 -0700

    blueprint: Lint all files

--- bot.js
+++ bot.js
@@ -1,20 +1,20 @@
-const {Container, Service, publicInternet} = require("@quilt/quilt");
+const { Container, Service, publicInternet } = require('@quilt/quilt');
 
-exports.New = function(github_oauth, google_json, slack_token) {
-    service = new Service("bot", [
-        new Container("quilt/bot")
-            .withEnv({
-                "GITHUB_OAUTH": github_oauth,
-                "SLACK_TOKEN": slack_token,
-            }).withFiles({
-                "/go/src/app/google_secret.json": google_json
-            })
-    ]);
+exports.New = function New(githubOauth, googleJson, slackToken) {
+  const service = new Service('bot', [
+    new Container('quilt/bot')
+      .withEnv({
+        GITHUB_OAUTH: githubOauth,
+        SLACK_TOKEN: slackToken,
+      }).withFiles({
+        '/go/src/app/google_secret.json': googleJson,
+      }),
+  ]);
 
-    publicInternet.connect(80, service);
-    service.connect(80, publicInternet);
-    service.connect(443, publicInternet);
-    service.connect(53, publicInternet);
+  publicInternet.connect(80, service);
+  service.connect(80, publicInternet);
+  service.connect(443, publicInternet);
+  service.connect(53, publicInternet);
 
-    return service;
-}
+  return service;
+};
